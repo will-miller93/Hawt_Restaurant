c390528bfc81bda6f7e8bd4604c4f7ddf2572300
Michael Gerov <mhgerov@gmail.com>
2018-04-13 08:06:22 -0400

    rename html, copy from demo

--- server.js
+++ server.js
@@ -15,28 +15,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Arrays for the data: 
+// Format:
+// [{
+// 		name: 'JohnQ',
+// 		size: 4,
+// 		phone: '678-777-2000'
+// 		email: 'john@example.com'
+// 	}]
 var resArr = [];
 var waitingList = [];
 // Routes
 // ============================================================
 
 // routes for getting and posting table data 
-app.get("/home", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
-// get to be able to get the information from the created reservation
-app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "reservationForm.html"));
-});
 
 // get to be able to get the reservations data
 app.get("/tables", function (req, res) {
-    res.sendFile(path.join(__dirname, "reservationView.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// GET reservation form
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 // post to be able to view the reservations
-app.post("/viewreservations", function (req, res) {
+app.post("/reserve", function (req, res) {
     var newReservation = req.body;
     // name, number, email, unique ID
     newReservation.routeName = newReservation.name
